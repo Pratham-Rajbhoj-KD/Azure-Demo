a0223f9974525b7111a6de3d006ba7c1b28866ed
Pratham-Rajbhoj-KD <prathameshrajendra.rajbhoj@kickdrumtech.com>
2024-01-13 00:00:32 +0530

    adding all files

---
+++ src/main/java/com/kdu/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.kdu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/")
+	public String index() {
+		return "Hello Prathamesh Rajbhoj, KD-24 batch (CLI)";
+	}
+}
+
+@RestController
+class HelloController1 {
+	@GetMapping("/hello")
+	public String index() {
+		return "Hello Prathamesh Rajbhoj, KD-24 batch (CLI)";
+	}
+}
+
